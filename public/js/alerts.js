a2253b8c04ad712aa8c289dd6faef572f1fbfc05
MostsfaMGomaa <gomaamostafa26@gmail.com>
2022-06-05 15:11:40 +0200

    After section 12

---
+++ public/js/alerts.js
@@ -0,0 +1,13 @@
+export const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+// Type : is success or fail
+export const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div calss="alert alert--${type}">${msg}</div>`;
+  `<div class="alert alert--${type}>${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
